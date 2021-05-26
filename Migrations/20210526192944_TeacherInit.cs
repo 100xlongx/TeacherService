4d464592809f063b89caeb6a6fcdbc2b370d08db
Raaed Khan <raaedk@gmail.com>
2021-05-26 15:33:48 -0400

    refactored service to use the db

---
+++ Migrations/20210526192944_TeacherInit.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Migrations
+{
+    public partial class TeacherInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TeachersInfos",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    teaPrefix = table.Column<string>(type: "TEXT", nullable: true),
+                    teaFirstName = table.Column<string>(type: "TEXT", nullable: true),
+                    teaLastName = table.Column<string>(type: "TEXT", nullable: true),
+                    teaAge = table.Column<int>(type: "INTEGER", nullable: false),
+                    teaDaySubject = table.Column<string>(type: "TEXT", nullable: true),
+                    teaOptionalLang = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeachersInfos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TeachersInfos");
+        }
+    }
+}
